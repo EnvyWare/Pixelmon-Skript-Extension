e47032063618fd3207d597462a78f202a052f61b
Daniel Norris <dnorris443@gmail.com>
2024-02-16 14:00:57 +0000

    feat(plugin): add SpeciesNameExpression

---
+++ forge16/src/main/java/com/envyful/pixelmon/skript/elements/exrepssion/SpeciesNameExpression.java
@@ -0,0 +1,26 @@
+package com.envyful.pixelmon.skript.elements.exrepssion;
+
+import ch.njol.skript.expressions.base.SimplePropertyExpression;
+import com.pixelmonmod.pixelmon.api.pokemon.Pokemon;
+
+public class SpeciesNameExpression extends SimplePropertyExpression<Pokemon, String> {
+
+    static {
+        register(SpeciesNameExpression.class, String.class, "species", "pokemon");
+    }
+
+    @Override
+    public String convert(Pokemon pokemon) {
+        return pokemon.getSpecies().getName();
+    }
+
+    @Override
+    protected String getPropertyName() {
+        return "species";
+    }
+
+    @Override
+    public Class<? extends String> getReturnType() {
+        return String.class;
+    }
+}
