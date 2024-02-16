298999bfa2b5243a0019575c38d4460d0b85b24d
Daniel Norris <dnorris443@gmail.com>
2024-02-16 14:35:12 +0000

    feat(plugin): add Spec class info

--- forge16/src/main/java/com/envyful/pixelmon/skript/elements/ClassInformation.java
+++ forge16/src/main/java/com/envyful/pixelmon/skript/elements/ClassInformation.java
@@ -5,7 +5,13 @@ import ch.njol.skript.classes.Parser;
 import ch.njol.skript.expressions.base.EventValueExpression;
 import ch.njol.skript.lang.ParseContext;
 import ch.njol.skript.registrations.Classes;
+import com.pixelmonmod.api.SpecificationFactory;
+import com.pixelmonmod.api.pokemon.PokemonSpecification;
+import com.pixelmonmod.api.pokemon.PokemonSpecificationProxy;
+import com.pixelmonmod.api.pokemon.requirement.impl.SpeciesRequirement;
 import com.pixelmonmod.pixelmon.api.pokemon.Pokemon;
+import com.pixelmonmod.pixelmon.api.registries.PixelmonRequirements;
+import com.pixelmonmod.pixelmon.entities.pixelmon.PixelmonEntity;
 
 public class ClassInformation {
 
@@ -36,6 +42,36 @@ public class ClassInformation {
                     @Override
                     public boolean canParse(ParseContext context) {
                         return false;
+                    }
+                }));
+        Classes.registerClass(new ClassInfo<>(PokemonSpecification.class, "spec")
+                .name("Spec")
+                .description("Represents a Pixelmon Spec")
+                .usage("spec")
+                .after("string")
+                .user("spec")
+                .defaultExpression(new EventValueExpression<>(PokemonSpecification.class))
+                .parser(new Parser<>() {
+                    @Override
+                    public PokemonSpecification parse(String s, ParseContext context) {
+                        var spec = PokemonSpecificationProxy.create(s);
+                        var requirements = SpecificationFactory.requirements(PokemonSpecification.class, s);
+
+                        if (requirements.isEmpty()) {
+                            return null;
+                        }
+
+                        return spec;
+                    }
+
+                    @Override
+                    public String toString(PokemonSpecification pokemon, int flags) {
+                        return toVariableNameString(pokemon);
+                    }
+
+                    @Override
+                    public String toVariableNameString(PokemonSpecification pokemon) {
+                        return "spec-" + pokemon.toString();
                     }
                 }));
     }
