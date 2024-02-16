70734fd83666a0bc5378e6a8604376e91ec7e2e6
Daniel Norris <dnorris443@gmail.com>
2024-02-16 14:56:59 +0000

    feat(plugin): add formatted dex and dex expressions

---
+++ forge16/src/main/java/com/envyful/pixelmon/skript/elements/exrepssion/FormattedPokemonDexExpression.java
@@ -0,0 +1,26 @@
+package com.envyful.pixelmon.skript.elements.exrepssion;
+
+import ch.njol.skript.expressions.base.SimplePropertyExpression;
+import com.pixelmonmod.pixelmon.api.pokemon.Pokemon;
+
+public class FormattedPokemonDexExpression extends SimplePropertyExpression<Pokemon, String> {
+
+    static {
+        register(FormattedPokemonDexExpression.class, String.class, "formatteddex", "pokemon");
+    }
+
+    @Override
+    public String convert(Pokemon pokemon) {
+        return pokemon.getSpecies().getFormattedDex();
+    }
+
+    @Override
+    protected String getPropertyName() {
+        return "formatteddex";
+    }
+
+    @Override
+    public Class<? extends String> getReturnType() {
+        return String.class;
+    }
+}
---
+++ forge16/src/main/java/com/envyful/pixelmon/skript/elements/exrepssion/PokemonDexExpression.java
@@ -0,0 +1,26 @@
+package com.envyful.pixelmon.skript.elements.exrepssion;
+
+import ch.njol.skript.expressions.base.SimplePropertyExpression;
+import com.pixelmonmod.pixelmon.api.pokemon.Pokemon;
+
+public class PokemonDexExpression extends SimplePropertyExpression<Pokemon, Integer> {
+
+    static {
+        register(PokemonDexExpression.class, Integer.class, "dex", "pokemon");
+    }
+
+    @Override
+    public Integer convert(Pokemon pokemon) {
+        return pokemon.getSpecies().getDex();
+    }
+
+    @Override
+    protected String getPropertyName() {
+        return "dex";
+    }
+
+    @Override
+    public Class<? extends Integer> getReturnType() {
+        return Integer.class;
+    }
+}
